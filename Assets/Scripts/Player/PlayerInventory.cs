a6b00481ffd0f9e0777760de8a063cfbb53c7416
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-04-16 14:20:23 +0200

    Weapon drop physics implemented

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -25,6 +25,8 @@ public class PlayerInventory : MonoBehaviour
             newWeapon.layer = LayerMask.NameToLayer("Interactable");
             SetLayerRecursively(newWeapon, LayerMask.NameToLayer("Interactable"));
             RemoveItem(newItemIndex);
+            Rigidbody weaponRigidbody = newWeapon.AddComponent<Rigidbody>();
+            weaponRigidbody.AddForce(transform.forward * 3f, ForceMode.Impulse);
         }
 
         weapons[newItemIndex] = newItem;
@@ -42,8 +44,16 @@ public class PlayerInventory : MonoBehaviour
         obj.layer = layer;
 
         foreach (Transform child in obj.transform)
-        {
             SetLayerRecursively(child.gameObject, layer);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collidedObject = collision.gameObject;
+
+        if (collidedObject.layer == LayerMask.NameToLayer("Interactable"))
+        {
+            Destroy(collidedObject.GetComponent<Rigidbody>());
+            collidedObject.transform.position = collision.contacts[0].point;
         }
     }
 }
