ac094e332f34b3fb46045e2c34d88c9f10e9eec5
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-04-18 22:24:50 +0200

    Ammo initialization added

---
+++ Assets/Scripts/InitializeAmmo.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class InitializeAmmo : MonoBehaviour
+{
+    public Gun[] guns = new Gun[6];
+
+    void Start()
+    {
+        foreach (var gun in guns)
+        {
+            if (gun.name.Contains("Pistol"))
+            {
+                gun.magazineSize = 12;
+                gun.currentAmmoCount = 12;
+                gun.maxAmmoCount = 60;
+            }else if (gun.name.Contains("Rifle"))
+            {
+                gun.magazineSize = 30;
+                gun.currentAmmoCount = 30;
+                gun.maxAmmoCount = 120;
+            }
+        }
+    }
+}
