c0cc1a453b58cd8817026e439a5c087c2b7da95a
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-04-19 22:42:36 +0200

    Ammo QoL changes implemented

--- Assets/Scripts/Interactables/Ammo.cs
+++ Assets/Scripts/Interactables/Ammo.cs
@@ -1,7 +1,15 @@
+using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class Ammo : Interactable
 {
+    [SerializeField]
+    private TextMeshProUGUI ammoRefillPrompt;
+    private float fadeDuration = 1.0f;
+    private Coroutine hideCoroutine;
+    private const int maxLines = 4;
+
     protected override void Interact()
     {
         PlayerInventory inventory = FindObjectOfType<PlayerInventory>();
@@ -14,8 +22,48 @@ public class Ammo : Interactable
                 {
                     if (gun.maxAmmoCount < gun.magazineSize * 4)
                         gun.maxAmmoCount += gun.magazineSize;
+                    else
+                        ShowAmmoRefillPrompt(gun.gunName);
                 }
             }
         }
+    }
+    private void ShowAmmoRefillPrompt(string gunName)
+    {
+        ammoRefillPrompt.text = "You cannot carry more " + gunName + " ammo!\n" + ammoRefillPrompt.text;
+        string[] lines = ammoRefillPrompt.text.Split('\n');
+
+        if (lines.Length > maxLines)
+        {
+            string newText = "";
+
+            for (int i = 0; i < maxLines; i++)
+                newText += lines[i] + "\n";
+
+            ammoRefillPrompt.text = newText;
+        }
+        if (hideCoroutine != null)
+            StopCoroutine(hideCoroutine);
+
+        hideCoroutine = StartCoroutine(HideAmmoRefillPrompt());
+    }
+    private IEnumerator HideAmmoRefillPrompt()
+    {
+        Color textColor = ammoRefillPrompt.color;
+        float startTime = Time.time;
+        float endTime = startTime + fadeDuration;
+
+        while (Time.time < endTime)
+        {
+            float t = (Time.time - startTime) / fadeDuration;
+            textColor.a = Mathf.Lerp(1.0f, 0.0f, t);
+            ammoRefillPrompt.color = textColor;
+            yield return null;
+        }
+
+        ammoRefillPrompt.text = "";
+        textColor.a = 1.0f;
+        ammoRefillPrompt.color = textColor;
+        hideCoroutine = null;
     }
 }
