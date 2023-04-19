103bc2f5b739c58386df4fdb1b4f66b033f22f7c
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-04-19 16:34:42 +0200

    Simple ammo refill implemented

---
+++ Assets/Scripts/Interactables/Ammo.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Ammo : Interactable
+{
+    protected override void Interact()
+    {
+        PlayerInventory inventory = FindObjectOfType<PlayerInventory>();
+
+        if (inventory != null)
+        {
+            foreach (Gun gun in inventory.weapons)
+            {
+                if (gun != null && gun.gunStyle != GunStyle.Melee)
+                    gun.maxAmmoCount += gun.magazineSize * 2;
+            }
+        }
+    }
+}
