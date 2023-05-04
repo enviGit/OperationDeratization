6b3a4a9e96ce2c9a9b535223e5d8fb025631c762
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-05-04 23:05:55 +0200

    Hitboxes system implemented

--- Assets/Scripts/Enemy/HitBox.cs
+++ Assets/Scripts/Enemy/HitBox.cs
@@ -1,13 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HitBox : MonoBehaviour
 {
     public EnemyHealth health;
+    private Gun gun;
     public PlayerHealth playerHealth;
+    private Dictionary<string, float> damageMultiplier = new Dictionary<string, float>()
+    {
+        { "Hips", 1f },
+        { "LeftUpLeg", 0.5f },
+        { "LeftLeg", 0.5f },
+        { "LeftFoot", 0.2f },
+        { "RightUpLeg", 0.5f },
+        { "RightLeg", 0.5f },
+        { "RightFoot", 0.2f },
+        { "Spine2", 1f },
+        { "LeftArm", 1f },
+        { "LeftForeArm", 0.7f },
+        { "Head", 2.5f },
+        { "RightArm", 0.7f },
+        { "RightForeArm", 1f },
+    };
 
     public void OnRaycastHit(Gun weapon, Vector3 direction)
     {
-        health.DealDamage(Random.Range(weapon.minimumDamage, weapon.maximumDamage), direction);
+        gun = weapon;
+        int damage = GetDamageFromHitBox(gameObject);
+        health.DealDamage(damage, direction);
     }
     public void OnExplosion(int damage)
     {
@@ -15,10 +35,21 @@ public class HitBox : MonoBehaviour
     }
     public void OnRaycastHitPlayer(Gun weapon)
     {
-        playerHealth.TakeDamage(Random.Range(weapon.minimumDamage, weapon.maximumDamage));
+        gun = weapon;
+        int damage = GetDamageFromHitBox(gameObject);
+        playerHealth.TakeDamage(damage);
     }
     public void OnExplosionPlayer(int damage)
     {
         playerHealth.TakeDamage(damage);
+    }
+    private int GetDamageFromHitBox(GameObject hitBoxObject)
+    {
+        string boneName = hitBoxObject.name;
+
+        if (damageMultiplier.ContainsKey(boneName))
+            return Mathf.RoundToInt(Random.Range(gun.minimumDamage, gun.maximumDamage) * damageMultiplier[boneName]);
+
+        return Random.Range(gun.minimumDamage, gun.maximumDamage);
     }
 }
