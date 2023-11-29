9909d0eafbc626b7e7243c6dae4c864d2f118aed
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-11-29 14:49:31 +0100

    Tracker visibility bug fixed, gas dmg added

--- Assets/Scripts/Interactables/GasParticleTrigger.cs
+++ Assets/Scripts/Interactables/GasParticleTrigger.cs
@@ -3,56 +3,21 @@ using System.Collections.Generic;
 
 public class GasParticleTrigger : MonoBehaviour
 {
-    private bool playerInside = false;
     private PlayerHealth playerHealth;
 
-    void OnTriggerEnter(Collider other)
+    private void OnParticleCollision(GameObject other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player entered trigger");
-            playerInside = true;
             playerHealth = other.GetComponent<PlayerHealth>();
+
             if (playerHealth != null)
-            {
                 InvokeRepeating("DealGasDamage", 0f, 0.5f);
-            }
         }
     }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player exited trigger");
-            playerInside = false;
-            CancelInvoke("DealGasDamage");
-        }
-    }
-
-    void OnParticleTrigger()
-    {
-        List<ParticleSystem.Particle> enterParticles = new List<ParticleSystem.Particle>();
-        int numEnterParticles = GetComponent<ParticleSystem>().GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enterParticles);
-
-        Debug.Log("Particle trigger event. Num particles: " + numEnterParticles);
-
-        for (int i = 0; i < numEnterParticles; i++)
-        {
-            // Do something when a particle enters the trigger zone
-            if (playerInside)
-            {
-                Debug.Log("Dealing gas damage");
-                DealGasDamage();
-            }
-        }
-    }
-
-    void DealGasDamage()
+    private void DealGasDamage()
     {
         if (playerHealth != null)
-        {
-            playerHealth.TakeGasDamage(Random.Range(11, 24));
-        }
+            playerHealth.TakeGasDamage(Random.Range(3, 15));
     }
 }
