527ca33017eafe61216f16b853ecf15c5af530a9
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-12-02 15:55:20 +0100

    FPS counter added, V-Sync disabled by default

---
+++ Assets/Scripts/UI/Fps.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class Fps : MonoBehaviour
+{
+    private float count;
+
+    private IEnumerator Start()
+    {
+        GUI.depth = 2;
+
+        while (true)
+        {
+            count = 1f / Time.unscaledDeltaTime;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    private void OnGUI()
+    {
+        GUIStyle headStyle = new GUIStyle();
+        headStyle.fontSize = 30;
+        headStyle.normal.textColor = Color.white;
+        GUI.Label(new Rect(5, 5, 100, 25), "FPS: " + Mathf.Round(count), headStyle);
+    }
+}
