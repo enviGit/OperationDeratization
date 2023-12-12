be41692fd5532a8f482c8288afcc7cca5da914dd
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-12-12 22:39:31 +0100

    Prefabs of interactables updated

--- Assets/Scripts/UI/Menu/WindowManager.cs
+++ Assets/Scripts/UI/Menu/WindowManager.cs
@@ -80,6 +80,10 @@ public class WindowManager : MonoBehaviour
     }
     public void QuitGame()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
