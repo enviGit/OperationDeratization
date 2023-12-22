77f3674f75133967964f43556ee68fe8ed208415
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-12-22 01:05:22 +0100

    Sun rotation and intensity changes added

--- Assets/Scripts/Manager/SkyController.cs
+++ Assets/Scripts/Manager/SkyController.cs
@@ -11,18 +11,21 @@ public class SkyController : MonoBehaviour
     [Header("Sun Settings")]
     [SerializeField] private Light sun;
     [SerializeField] private float dayDuration = 300f;
-    //[SerializeField] private float sunRotationSpeed = 0.1f;
 
     private void Update()
     {
         if (Time.timeScale != 0f)
         {
             elapsedTime += Time.deltaTime;
-            skybox.SetFloat(Rotation, elapsedTime);
+            float cloudsRotationSpeed = 0.2f;
+            skybox.SetFloat(Rotation, elapsedTime * cloudsRotationSpeed);
             float normalizedTime = elapsedTime / dayDuration;
             float targetExposure = Mathf.Lerp(0.15f, 1f, Mathf.Sin(normalizedTime * Mathf.PI * 2) * 0.5f + 0.5f);
             skybox.SetFloat(Exposure, targetExposure);
-            //sun.transform.RotateAround(Vector3.zero, Vector3.right, normalizedTime * sunRotationSpeed);
+            float sunRotationSpeed = 0.2f;
+            sun.transform.rotation = Quaternion.Euler(130f, normalizedTime * 360f * sunRotationSpeed, 0f);
+            float intensity = Mathf.Lerp(0.15f, 0.5f, targetExposure);
+            sun.intensity = intensity;
         }
     }
 }
