db8fbd216d6102a47ae7507504da183538f34041
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-12-22 02:13:51 +0100

    Temporary game over screen implemented

--- Assets/Scripts/UI/Menu/PauseMenu.cs
+++ Assets/Scripts/UI/Menu/PauseMenu.cs
@@ -1,19 +1,19 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-    public GameObject pauseMenuUI;
-    private PlayerHealth playerStatus;
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private PlayerHealth playerStatus;
+    [SerializeField] private GameObject endGameScreen;
+    [SerializeField] private TextMeshProUGUI endGameText;
 
-    private void Start()
-    {
-        playerStatus = FindObjectOfType<PlayerHealth>();
-    }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && playerStatus.isAlive)
         {
             if (GameIsPaused)
                 Resume();
@@ -29,6 +29,13 @@ public class PauseMenu : MonoBehaviour
                 if (child != transform && !child.name.Contains("Menu") && !IsPartOfMenu(child))
                     child.gameObject.SetActive(false);
             }
+
+            endGameScreen.SetActive(true);
+            endGameText.text = "GAME   OVER";
+            Transform endGameChild = endGameScreen.transform.GetChild(0).transform;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            StartCoroutine(ActivateChildrenRandomly(endGameChild));
         }
     }
     private bool IsPartOfMenu(Transform obj)
@@ -53,14 +60,10 @@ public class PauseMenu : MonoBehaviour
         AudioListener.pause = false;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
-        if (playerStatus.isAlive)
-        {
-            PlayerShoot pointer = FindObjectOfType<PlayerShoot>();
-            pointer.enabled = true;
-            PlayerInventory inventory = FindObjectOfType<PlayerInventory>();
-            inventory.enabled = true;
-        }
+        PlayerShoot pointer = FindObjectOfType<PlayerShoot>();
+        pointer.enabled = true;
+        PlayerInventory inventory = FindObjectOfType<PlayerInventory>();
+        inventory.enabled = true;
     }
     private void Pause()
     {
@@ -97,5 +100,48 @@ public class PauseMenu : MonoBehaviour
 #else
         Application.Quit();
 #endif
+    }
+    private IEnumerator ActivateChildrenRandomly(Transform parent)
+    {
+        int childCount = parent.childCount;
+        bool[] activatedChildren = new bool[childCount];
+
+        while (!AllChildrenActivated(activatedChildren))
+        {
+            int randomChildIndex = GetRandomChildIndex(childCount, activatedChildren);
+
+            if (randomChildIndex != -1)
+            {
+                Transform child = parent.GetChild(randomChildIndex);
+                child.gameObject.SetActive(true);
+                activatedChildren[randomChildIndex] = true;
+
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+    }
+    private int GetRandomChildIndex(int childCount, bool[] activatedChildren)
+    {
+        int randomIndex = Random.Range(0, childCount);
+
+        for (int i = 0; i < childCount; i++)
+        {
+            int index = (randomIndex + i) % childCount;
+
+            if (!activatedChildren[index])
+                return index;
+        }
+
+        return -1;
+    }
+    private bool AllChildrenActivated(bool[] activatedChildren)
+    {
+        foreach (bool activated in activatedChildren)
+        {
+            if (!activated)
+                return false;
+        }
+
+        return true;
     }
 }
