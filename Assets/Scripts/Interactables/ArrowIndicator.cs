6d52ed5b3603fe065ed7bf968224f959b3b45f8b
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2024-01-19 21:37:17 +0100

    First aid kit and DOTween bugfixes

--- Assets/Scripts/Interactables/ArrowIndicator.cs
+++ Assets/Scripts/Interactables/ArrowIndicator.cs
@@ -7,7 +7,7 @@ namespace RatGamesStudios.OperationDeratization
     {
         private Transform player;
         private Quaternion initialRotation;
-        private bool isDestroyed = false;
+        private Sequence moveSequence;
         public float moveDistance = 1f;
         public float moveAnimationDuration = 1f;
         public float rotateAnimationDuration = 0.5f;
@@ -16,37 +16,35 @@ namespace RatGamesStudios.OperationDeratization
         {
             player = GameObject.FindGameObjectWithTag("Player").transform;
             initialRotation = transform.rotation;
-            Invoke("PlayArrowAnimation", 0.01f);
+            PlayArrowAnimation();
         }
         private void PlayArrowAnimation()
         {
-            if (this != null && gameObject != null)
-            {
-                // Kill any existing tweens on this object
-                DOTween.Kill(transform);
-                // Move the arrow up and down alternately
-                Sequence moveSequence = DOTween.Sequence();
-                moveSequence.AppendCallback(() => initialRotation = transform.rotation);
-                moveSequence.Append(transform.DOMoveY(transform.position.y + moveDistance, moveAnimationDuration)
-                    .SetEase(Ease.Linear)
-                    .OnUpdate(() =>
-                    {
+            // Stop and clear tweens if they exist
+            DOTween.Kill(transform);
+            // Move the arrow up and down alternately
+            moveSequence = DOTween.Sequence();
+            moveSequence.AppendCallback(() => initialRotation = transform.rotation);
+            moveSequence.Append(transform.DOMoveY(transform.position.y + moveDistance, moveAnimationDuration)
+                .SetEase(Ease.Linear)
+                .OnUpdate(() =>
+                {
                         // Rotate the arrow towards the player's position while moving
                         transform.LookAt(player.position);
                         // Apply the initial rotation to maintain the relative rotation
                         transform.rotation = Quaternion.Euler(90f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-                    }));
+                }));
 
-                // Set the infinite loops directly on the sequence
-                moveSequence.SetLoops(-1, LoopType.Yoyo);
-                moveSequence.Play();
-            }
+            // Set the infinite loops directly on the sequence
+            moveSequence.SetLoops(-1, LoopType.Yoyo);
+            moveSequence.Play();
         }
         // Called when the parent object is destroyed
         private void OnDestroy()
         {
-            // Kill any existing tweens on this object
-            DOTween.Kill(transform);
+            // Check if tweens sequence is active and stop it
+            if (moveSequence != null && moveSequence.IsActive())
+                moveSequence.Kill();
         }
     }
 }
