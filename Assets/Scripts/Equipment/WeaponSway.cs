aa79387d79131e000c115cafc1cff389c2dde4cd
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2024-02-12 02:07:46 +0100

    Bug fixes
    Rifle near old building bug fixed
    Church bug fixed
    Weapon sway removed

--- Assets/Scripts/Equipment/WeaponSway.cs
+++ Assets/Scripts/Equipment/WeaponSway.cs
@@ -11,23 +11,15 @@ namespace RatGamesStudios.OperationDeratization.Equipment
         [SerializeField] private PlayerMotor playerMotor;
         [SerializeField] private PlayerShoot playerShoot;
 
-        [Header("Sway and bob")]
-        public float step = 0.01f;
-        public float maxStepDistance = 0.06f;
-        public float rotationStep = 4f;
-        public float maxRotationStep = 5f;
+        [Header("Sway")]
         public float speedCurve;
         public Vector3 travelLimit = Vector3.one * 0.025f;
         public Vector3 bobLimit = Vector3.one * 0.01f;
         public Vector3 multiplier = Vector3.one * 1.5f;
-        private float smooth = 10f;
         private float smoothRot = 12f;
         private float curveSin { get => Mathf.Sin(speedCurve); }
         private float curveCos { get => Mathf.Cos(speedCurve); }
         private Vector2 walkInput;
-        private Vector2 lookInput;
-        private Vector3 swayPos;
-        private Vector3 swayEulerRot;
         private Vector3 bobPosition;
         private Vector3 bobEulerRotation;
         private float timeScale;
@@ -56,8 +48,6 @@ namespace RatGamesStudios.OperationDeratization.Equipment
                 multiplier = Vector3.one * 0f;
 
             GetInput();
-            Sway();
-            SwayRotation();
             BobOffset();
             BobRotation();
             CompositePositionRotation();
@@ -67,29 +57,11 @@ namespace RatGamesStudios.OperationDeratization.Equipment
             walkInput.x = Input.GetAxisRaw("Horizontal");
             walkInput.y = Input.GetAxisRaw("Vertical");
             walkInput = walkInput.normalized;
-            lookInput.x = Input.GetAxisRaw("Mouse X");
-            lookInput.y = Input.GetAxisRaw("Mouse Y");
-        }
-        private void Sway()
-        {
-            Vector3 invertLook = lookInput * -step;
-            invertLook.x = Mathf.Clamp(invertLook.x, -maxStepDistance, maxStepDistance);
-            invertLook.x = Mathf.Clamp(invertLook.y, -maxStepDistance, maxStepDistance);
-            swayPos = invertLook;
-        }
-        private void SwayRotation()
-        {
-            Vector2 invertLook = lookInput * -rotationStep;
-            invertLook.x = Mathf.Clamp(invertLook.x, -maxRotationStep, maxRotationStep);
-            invertLook.x = Mathf.Clamp(invertLook.y, -maxRotationStep, maxRotationStep);
-            swayEulerRot = new Vector3(invertLook.y, invertLook.x, invertLook.x);
         }
         private void CompositePositionRotation()
         {
             float deltaTime = Time.deltaTime;
-
-            transform.localPosition = Vector3.Lerp(transform.localPosition, swayPos, deltaTime * smooth * Time.timeScale);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(swayEulerRot) * Quaternion.Euler(bobEulerRotation), deltaTime * smoothRot * Time.timeScale);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(bobEulerRotation), deltaTime * smoothRot * Time.timeScale);
         }
         private void BobOffset()
         {
@@ -100,7 +72,6 @@ namespace RatGamesStudios.OperationDeratization.Equipment
             bobPosition.y = (curveSin * bobLimit.y) - (rb.velocity.y * travelLimit.y);
             bobPosition.z = -(walkInput.y * travelLimit.z);
         }
-
         private void BobRotation()
         {
             bobEulerRotation.x = (walkInput != Vector2.zero ? multiplier.x * (Mathf.Sin(2 * speedCurve)) : multiplier.x * (Mathf.Sin(2 * speedCurve) / 2));
